061696685ecf37ae39de5b76f5bcb86b4a7a08d2
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-08-25 14:30:45 +0700

    renew

--- app/Views/home.php
+++ app/Views/home.php
@@ -381,7 +381,7 @@
               </div>
             </div>
 
-            <div class="col-lg-8 mt-5 mt-lg-0">
+            <div class="col-lg-12 mt-5 mt-lg-0">
               <form action="forms/contact.php" method="post" role="form" class="php-email-form">
                 <div class="row">
                   <div class="col-md-6 form-group">
